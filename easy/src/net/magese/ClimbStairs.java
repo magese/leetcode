e7368edc2d4813b3c99aef1863b96457c0eb0664
magese <magese@live.cn>
2018-05-30 10:51:48 +0800

    Initial commit

---
+++ easy/src/net/magese/ClimbStairs.java
@@ -0,0 +1,54 @@
+package net.magese;
+
+import java.util.Scanner;
+
+/**
+ * 假设你正在爬楼梯。需要 n 步你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ * 注意：给定 n 是一个正整数。
+ *
+ * 示例 1：
+ *
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 步 + 1 步
+ * 2.  2 步
+ * 示例 2：
+ *
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 步 + 1 步 + 1 步
+ * 2.  1 步 + 2 步
+ * 3.  2 步 + 1 步
+ */
+public class ClimbStairs {
+    private static int climbStairs(int n) {
+        /*
+        1 = 1
+        2 = 2
+        3 = 3
+        4 = 5
+        5 = 8
+        斐波那契数列
+         */
+        int x = 1, y = 0;
+        if (n == x || n == y)
+            return 1;
+        for (int i = 1; i <= n; i++) {
+            int temp = x;
+            x = x + y;
+            y = temp;
+        }
+        return x;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("共有" + climbStairs(sc.nextInt()) + "种方法可以爬到楼顶。");
+        sc.close();
+    }
+}
---
+++ easy/src/net/magese/TowSum.java
@@ -0,0 +1,36 @@
+package net.magese;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+ *
+ * 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+ *
+ * 示例:
+ *
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ *
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ */
+public class TowSum {
+    private static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int r = target - nums[i];
+            if (map.containsKey(r)) {
+                return new int[] {map.get(r), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        System.out.println(Arrays.toString(twoSum(nums, 9)));
+    }
+}
