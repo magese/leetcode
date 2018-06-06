ffd12d5b6b37733169846d8fb0a058d44d324d1c
magese <magese@live.cn>
2018-06-06 10:03:55 +0800

    字符串转整数 (atoi)

--- easy/src/net/magese/ATOI.java
+++ easy/src/net/magese/ATOI.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 /**
  * <a>https://leetcode-cn.com/problems/string-to-integer-atoi/description/</a>
- *
+ * <p>
  * 实现 atoi，将字符串转为整数。
  * <p>
  * 在找到第一个非空字符之前，需要移除掉字符串中的空格字符。如果第一个非空字符是正号或负号，选取该符号，并将其与后面尽可能多的连续的数字组合起来，这部分字符即为整数的值。如果第一个非空字符是数字，则直接将其与之后连续的数字字符组合起来，形成整数。
@@ -49,41 +49,54 @@ import java.util.Scanner;
  */
 public class ATOI {
     private static int myAtoi(String str) {
+        // 将源字符串转换为目标数字字符串
         str = str.trim();
-        int res = 0;
-        if (str.length() > 0) {
-            String frist = str.substring(0, 1);
-            StringBuilder num = new StringBuilder();
-            if (!frist.matches("[\\-+0-9]"))
-                return res;
-            for (int i = 0; i < str.length(); i++) {
-                char c = str.charAt(i);
-                if (c >= 48 && c <= 57)
-                    num.append(c);
-                else if (num.length() > 0)
-                    break;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c >= '0' && c <= '9')
+                builder.append(c);
+            else if ((c == '+' || c == '-') && i == 0)
+                builder.append(c);
+            else
+                break;
+        }
+
+        if (builder.length() == 0)
+            return 0;
+        if (builder.charAt(0) == '+' || builder.charAt(0) == '-') {
+            for (int i = 1; i < builder.length(); i++) {
+                if (builder.charAt(i) == '0')
+                    builder.deleteCharAt(i--);
+                else break;
             }
-            if (num.length() < 1)
-                return res;
-            if (frist.equals("+") || frist.equals("-"))
-                num.insert(0, frist);
-            long l;
-            try {
-                l = Long.parseLong(num.toString());
-            } catch (Exception e) {
-                if (frist.equals("-"))
+            if (builder.length() == 1)
+                return 0;
+        } else {
+            for (int i = 0; i < builder.length(); i++) {
+                if (builder.charAt(i) == '0')
+                    builder.deleteCharAt(i--);
+                else break;
+            }
+        }
+
+        if (builder.length() > 0) {
+            if (builder.length() <= 11) {
+                long l = Long.parseLong(builder.toString());
+                if (l > Integer.MAX_VALUE)
+                    return Integer.MAX_VALUE;
+                else if (l < Integer.MIN_VALUE)
                     return Integer.MIN_VALUE;
                 else
-                    return Integer.MAX_VALUE;
+                    return (int) l;
+            } else if (builder.charAt(0) == '-') {
+                return Integer.MIN_VALUE;
+            } else {
+                return Integer.MAX_VALUE;
             }
-            if (l > Integer.MAX_VALUE)
-                res = Integer.MAX_VALUE;
-            else if (l < Integer.MIN_VALUE)
-                res = Integer.MIN_VALUE;
-            else
-                res = (int) l;
+        } else {
+            return 0;
         }
-        return res;
     }
 
     public static void main(String[] args) {
