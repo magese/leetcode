80755dc596c07374f61af481c4b34d4f4dbfc7e8
magese <magese@live.cn>
2018-07-16 10:30:36 +0800

    移除元素；

--- easy/src/net/magese/RemoveElement.java
+++ easy/src/net/magese/RemoveElement.java
@@ -60,7 +60,7 @@ public class RemoveElement {
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{0,1,2,2,3,0,4,2};
+        int[] nums = new int[]{0, 1, 2, 2, 3, 0, 4, 2};
         int val = 2;
         System.out.println(removeElement(nums, val));
         System.out.println(Arrays.toString(nums));
