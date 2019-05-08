d3bf9fcfb3d07630b8ac44ff06d912a4983f64c1
magese <magese@live.cn>
2019-05-08 09:58:40 +0800

    合并两个有序数组；

---
+++ medium/src/net/magese/UniqueBinarySearchTrees.java
@@ -0,0 +1,56 @@
+package net.magese;
+
+/**
+ * <a>https://leetcode-cn.com/problems/unique-binary-search-trees/</a>
+ *
+ * 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+ *
+ * 示例:
+ *
+ * 输入: 3
+ * 输出: 5
+ * 解释:
+ * 给定 n = 3, 一共有 5 种不同结构的二叉搜索树:
+ *
+ *    1         3     3      2      1
+ *     \       /     /      / \      \
+ *      3     2     1      1   3      2
+ *     /     /       \                 \
+ *    2     1         2                 3
+ */
+public class UniqueBinarySearchTrees {
+    // f(3) = f(1 - 1) * f(3 - 1) + f(2 - 1) * f(3 - 2) + f(3 - 1) * f(3 - 3)
+    private static int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < dp.length; i++) {
+            for (int j = 1; j < i + 1; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(numTrees(n));
+    }
+}
+/*
+结题思路：假设n个节点存在二叉排序树的个数是G(n)，1为根节点，2为根节点，...，n为根节点，当1为根节点时，其左子树节点个数为0，
+右子树节点个数为n-1，同理当2为根节点时，其左子树节点个数为1，右子树节点为n-2，所以可得G(n) = G(0)*G(n-1)+G(1)*(n-2)+...+G(n-1)*G(0)
+
+
+思路:
+
+动态规划
+
+假设n个节点存在二叉排序树的个数是G(n)，令f(i)为以i为根的二叉搜索树的个数
+
+即有:G(n) = f(1) + f(2) + f(3) + f(4) + ... + f(n)
+
+n为根节点，当i为根节点时，其左子树节点个数为[1,2,3,...,i-1]，右子树节点个数为[i+1,i+2,...n]，所以当i为根节点时，其左子树节点个数为i-1个，右子树节点为n-i，即f(i) = G(i-1)*G(n-i),
+
+上面两式可得:G(n) = G(0)*G(n-1)+G(1)*(n-2)+...+G(n-1)*G(0)
+ */
