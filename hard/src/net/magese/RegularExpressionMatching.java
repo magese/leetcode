341fae824290ca45c8a025cc82ee2f5c06de4ab4
magese <magese@live.cn>
2019-05-09 09:12:58 +0800

    10. 正则表达式匹配

--- hard/src/net/magese/RegularExpressionMatching.java
+++ hard/src/net/magese/RegularExpressionMatching.java
@@ -1,6 +1,8 @@
 package net.magese;
 
 /**
+ * 10. 正则表达式匹配
+ *
  * <a>https://leetcode-cn.com/problems/regular-expression-matching/description/</a>
  * <p>
  * 给定一个字符串 (s) 和一个字符模式 (p)。实现支持 '.' 和 '*' 的正则表达式匹配。
