c90f5973c900d7fc68a136cf5605718bcb529e54
magese <magese@live.cn>
2019-05-09 09:11:17 +0800

    15. 三数之和

--- medium/src/net/magese/ThreeSum.java
+++ medium/src/net/magese/ThreeSum.java
@@ -3,18 +3,20 @@ package net.magese;
 import java.util.*;
 
 /**
+ * 15. 三数之和
+ *
  * <a>https://leetcode-cn.com/problems/3sum/description/</a>
- *
+ * <p>
  * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
- *
+ * <p>
  * 注意：答案中不可以包含重复的三元组。
- *
+ * <p>
  * 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
- *
+ * <p>
  * 满足要求的三元组集合为：
  * [
- *   [-1, 0, 1],
- *   [-1, -1, 2]
+ * [-1, 0, 1],
+ * [-1, -1, 2]
  * ]
  */
 public class ThreeSum {
@@ -27,7 +29,7 @@ public class ThreeSum {
             // 如果元素重复则跳过当次
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
-            for (int j = i + 1, k = nums.length - 1; j < k;) {
+            for (int j = i + 1, k = nums.length - 1; j < k; ) {
                 // 如果元素重复则跳过当次
                 if (j > i + 1 && nums[j - 1] == nums[j]) {
                     j++;
