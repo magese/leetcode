fe172a6dc43ce2c87a07268aea281940cc267b76
magese <magese@live.cn>
2019-05-09 09:07:46 +0800

    26. 删除排序数组中的重复项

--- easy/src/net/magese/RemoveDuplicatesFromSortedArray.java
+++ easy/src/net/magese/RemoveDuplicatesFromSortedArray.java
@@ -3,6 +3,8 @@ package net.magese;
 import java.util.Arrays;
 
 /**
+ * 26. 删除排序数组中的重复项
+ *
  * <a>https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/description/</a>
  * <p>
  * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
