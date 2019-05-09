cd5864e90a8ab6f1f3f1e0dbce94aae9c0c1e68b
magese <magese@live.cn>
2019-05-09 09:09:22 +0800

    35. 搜索插入位置

--- easy/src/net/magese/SearchInsertPosition.java
+++ easy/src/net/magese/SearchInsertPosition.java
@@ -1,6 +1,8 @@
 package net.magese;
 
 /**
+ * 35. 搜索插入位置
+ *
  * <a>https://leetcode-cn.com/problems/search-insert-position/description/</a>
  * <p>
  * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
