177614925d6afe430bbd0d1c5c27ed21fd9167e4
magese <magese@live.cn>
2019-05-09 09:08:15 +0800

    27. 移除元素

--- easy/src/net/magese/RemoveElement.java
+++ easy/src/net/magese/RemoveElement.java
@@ -3,6 +3,8 @@ package net.magese;
 import java.util.Arrays;
 
 /**
+ * 27. 移除元素
+ *
  * <a>https://leetcode-cn.com/problems/remove-element/description/</a>
  * <p>
  * 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
