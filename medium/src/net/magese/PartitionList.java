b6b917f6e57d416141c9a2f716a4d98da8242866
magese <magese@live.cn>
2019-05-15 11:06:08 +0800

    86. 分隔链表

--- medium/src/net/magese/PartitionList.java
+++ medium/src/net/magese/PartitionList.java
@@ -75,24 +75,3 @@ public class PartitionList {
         System.out.println(partitionList.partition(head, x));
     }
 }
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode(int x) {
-        val = x;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        ListNode loop = this;
-        while (loop != null) {
-            builder.append(loop.val);
-            loop = loop.next;
-            if (loop != null) builder.append("->");
-        }
-        return builder.toString();
-    }
-}
