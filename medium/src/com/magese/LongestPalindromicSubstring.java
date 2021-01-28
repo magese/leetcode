210355572d618639f1bfd77d8884319889c8f377
Magese <magese@live.cn>
2021-01-28 09:52:11 +0800

    5. 最长回文子串

---
+++ medium/src/com/magese/LongestPalindromicSubstring.java
@@ -0,0 +1,68 @@
+package com.magese;
+
+/**
+ * @author gaozhicheng
+ * @since 2021/1/4 17:23
+ *
+ * 5. 最长回文子串
+ *
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ *
+ * 示例 1：
+ *
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 示例 2：
+ *
+ * 输入: "cbbd"
+ * 输出: "bb"
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LongestPalindromicSubstring {
+
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        // 保存起始位置，测试了用数组似乎能比全局变量稍快一点
+        int[] range = new int[2];
+        char[] str = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            // 把回文看成中间的部分全是同一字符，左右部分相对称
+            // 找到下一个与当前字符不同的字符
+            i = findLongest(str, i, range);
+        }
+        return s.substring(range[0], range[1] + 1);
+    }
+
+    public static int findLongest(char[] str, int low, int[] range) {
+        // 查找中间部分
+        int high = low;
+        while (high < str.length - 1 && str[high + 1] == str[low]) {
+            high++;
+        }
+        // 定位中间部分的最后一个字符
+        int ans = high;
+        // 从中间向左右扩散
+        while (low > 0 && high < str.length - 1 && str[low - 1] == str[high + 1]) {
+            low--;
+            high++;
+        }
+        // 记录最大长度
+        if (high - low > range[1] - range[0]) {
+            range[0] = low;
+            range[1] = high;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String s = "slvafhpfjpbqbpcuwxuexavyrtymfydcnvvbvdoitsvumbsvoayefsnusoqmlvatmfzgwlhxtkhdnlmqmyjztlytoxontggyytcezredlrrimcbkyzkrdeshpyyuolsasyyvxfjyjzqksyxtlenaujqcogpqmrbwqbiaweacvkcdxyecairvvhngzdaujypapbhctaoxnjmwhqdzsvpyixyrozyaldmcyizilrmmmvnjbyhlwvpqhnnbausoyoglvogmkrkzppvexiovlxtmustooahwviluumftwnzfbxxrvijjyfybvfnwpjjgdudnyjwoxavlyiarjydlkywmgjqeelrohrqjeflmdyzkqnbqnpaewjdfmdyoazlznzthiuorocncwjrocfpzvkcmxdopisxtatzcpquxyxrdptgxlhlrnwgvee";
+        System.out.println(longestPalindrome(s));
+    }
+
+}
