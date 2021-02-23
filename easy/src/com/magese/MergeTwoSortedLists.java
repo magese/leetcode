e016c8b864e7cfafcc9845a715776e9630f42e5b
Magese <magese@live.cn>
2021-02-23 10:21:15 +0800

    21. 合并两个有序链表

---
+++ easy/src/com/magese/MergeTwoSortedLists.java
@@ -0,0 +1,82 @@
+package com.magese;
+
+import com.magese.common.ListNode;
+
+/**
+ * @author gaozhicheng
+ * @since 2021/2/23 9:54
+ *
+ * 21. 合并两个有序链表
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * 示例 2：
+ *
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ *
+ *
+ * 提示：
+ *
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100 <= Node.val <= 100
+ * l1 和 l2 均按 非递减顺序 排列
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MergeTwoSortedLists {
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        ListNode head;
+        if (l1.val < l2.val) {
+            head = new ListNode(l1.val);
+            l1 = l1.next;
+        } else {
+            head = new ListNode(l2.val);
+            l2 = l2.next;
+        }
+        ListNode curr = head;
+        while (true) {
+            if (l1 == null) {
+                curr.next = l2;
+                break;
+            }
+            if (l2 == null) {
+                curr.next = l1;
+                break;
+            }
+            if (l1.val < l2.val) {
+                curr.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                curr.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.setNext(new ListNode(2)).setNext(new ListNode(4));
+        ListNode l2 = new ListNode(0);
+        l2.setNext(new ListNode(3)).setNext(new ListNode(4));
+        System.out.println(mergeTwoLists(l1, l2));
+    }
+
+}
